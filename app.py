008959ccdd51d4939f007353e2e3374974fe122f
syedmrizwan <syed.m.rizwan@outlook.com>
2020-04-19 22:06:37 +0500

    Implemented update and delete

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://root:root@localhost:5432/c
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
 class CarsModel(db.Model):
     __tablename__ = 'cars'
 
@@ -30,7 +31,8 @@ def handle_cars():
     if request.method == 'POST':
         if request.is_json:
             data = request.get_json()
-            new_car = CarsModel(name=data['name'], model=data['model'], doors=data['doors'])
+            new_car = CarsModel(
+                name=data['name'], model=data['model'], doors=data['doors'])
             db.session.add(new_car)
             db.session.commit()
             return {"message": "car {} has been created successfully.".format(new_car.name)}
@@ -47,3 +49,30 @@ def handle_cars():
             } for car in cars]
 
         return {"count": len(results), "cars": results}
+
+
+@app.route('/cars/<car_id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_car(car_id):
+    car = CarsModel.query.get_or_404(car_id)
+
+    if request.method == 'GET':
+        response = {
+            "name": car.name,
+            "model": car.model,
+            "doors": car.doors
+        }
+        return {"message": "success", "car": response}
+
+    elif request.method == 'PUT':
+        data = request.get_json()
+        car.name = data['name']
+        car.model = data['model']
+        car.doors = data['doors']
+        db.session.add(car)
+        db.session.commit()
+        return {"message": "car {} successfully updated".format(car.name)}
+
+    elif request.method == 'DELETE':
+        db.session.delete(car)
+        db.session.commit()
+        return {"message": "Car {} successfully deleted.".format(car.name)}
